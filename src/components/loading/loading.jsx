e03d34679f2e590ef0b77f6096a83cfb7d6a6c20
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-07-03 22:35:15 -0300

    Ajustes gerais no css da página das Organizações

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -1,10 +1,13 @@
 import './loading.scss';
 import LoadingLogo from '../../assets/images/airy.svg';
+import Tilt from 'react-tilt';
 
 function Loading() {
   return (
     <div className="background">
-      <img src={LoadingLogo} alt="Loading Logo" className="loading-image" />
+      <Tilt className="Tilt" options={{ perspective: 2200, scale: 1.05 }}>
+        <img src={LoadingLogo} alt="Loading Logo" className="loading-image" />
+      </Tilt>
       <h1 className="loading-text">Carregando</h1>
     </div>
   );
