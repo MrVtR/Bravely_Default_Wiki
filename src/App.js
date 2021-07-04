48464d40d852c869fc47f323120b922a1c1f7d39
MrVtR <30987772+MrVtR@users.noreply.github.com>
2021-07-04 20:08:12 -0300

    Mudança de pastas para style do arquivo App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import {
   Personagens,
   Organizacoes,
   CharacterDetail,
+  Villains,
 } from './pages/exportPages';
-import './App.scss';
+import './style/App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Switch>
-        {/* <Route path="/detail" component={ChDetail} /> */}
         <Route path="/" exact component={Home} />
         <Route path="/personagens" exact component={Personagens} />
         <Route path="/organizacoes" exact component={Organizacoes} />
         <Route path="/detail" exact component={CharacterDetail} />
+        <Route path="/viloes" exact component={Villains} />
       </Switch>
     </Router>
   );
